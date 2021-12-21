8776c63cae280c1923dbe1843a513d1bfa90e0ad
Stephan Zuercher <szuercher@slack-corp.com>
2021-12-20 17:36:32 -0800

    basic framework and no-op first stage

---
+++ setup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# fail on unset variables
+set -u
+
+abort() {
+    printf "%s\n" "$@"
+    exit 1
+}
+
+push_dir() {
+    pushd "${1}" >/dev/null || abort "could not change to ${1}"
+}
+
+pop_dir() {
+    popd || abort "could not pop dir"
+}
+
+# Require bash
+# shellcheck disable=SC2292
+if [ -z "${BASH_VERSION:-}" ]; then
+    abort "bash is required to interpret this script."
+fi
+
+# Check if stdin is a terminal
+NOTERM=false
+if [ ! -t 0 ]; then
+    NOTERM=true
+fi
+
+OS="$(uname)"
+if [[ "$OS" != "Linux" ]]; then
+    abort "OS is ${OS} -- this isn't going to work out."
+fi
+
+DISTRIBUTION="$(lsb_release -si)"
+if [[ "$DISTRIBUTION" != "Raspbian" ]]; then
+    echo "Expected a Raspbian distribution, but we'll muddle on..."
+fi
+
+# Check if git is installed.
+if hash git >/dev/null 2>&1; then
+    # Nope. Tallyho!
+    echo "installing git"
+    sudo apt-get -y install git
+    hash -r
+
+    if hash git >/dev/null 2>&1; then
+        abort "tried to install git, but still can't find it on the path"
+    fi
+fi
+
+REPO="https://github.com/zuercher/bdr-pi"
+BDR_DIR="${HOME}/${USER}/.bdr-pi"
+if [[ -d "${BDR_DIR}/.git" ]]; then
+    # Git repository is present. Let's update it.
+    push_dir "${BDR_DIR}"
+    git pull || abort "unable to pull $(git remote get-url origin)"
+    pop_dir
+else
+    # No git repository. Clone it.
+    git clone "${REPO}" "${BDR_DIR}" || abort "unable to clone ${REPO}"
+fi
+
+mkdir -p "${BDR_DIR}/state" || abort "could not create state dir"
+
+# Initial setup is complete, now transfer control to the code in BDR_DIR
+export BDR_DIR
+"${BDR_DIR}/update.sh" "$@"
---
+++ update.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+# fail on unset variables
+set -u
+
+abort() {
+    printf "%s\n" "$@"
+    exit 1
+}
+
+# prompt $1=default-value $2... prompt
+prompt() {
+    local ANSWER
+    local DEFAULT="$1"
+    shift
+    read -er -p "$*" ANSWER
+    if [ -z "${ANSWER}" ]; then
+        ANSWER="${DEFAULT}"
+    fi
+    echo "${ANSWER}"
+}
+
+push_dir() {
+    pushd "${1}" >/dev/null || abort "could not change to ${1}"
+}
+
+pop_dir() {
+    popd || abort "could not pop dir"
+}
+
+check_stage() {
+    local STAGE_NAME="$1"
+    local STATE_FILE="${STATE_DIR}/${STAGE_NAME}"
+    if [[ ! -f "${STATE_FILE}" ]]; then
+        return 1
+    fi
+
+    NOW=$(date "+%s")
+    TS="$( grep -E '^[0-9]+$' "${STATE_FILE}" )"
+
+    if [[ -z "${TS}" ||  "${TS}" -gt "${NOW}" ]]; then
+        echo "state file is too new or invalid, rerunning stage ${STAGE_NAME}"
+        return 1
+    fi
+
+    if "${FORCE}"; then
+        echo "rerunning stage ${STAGE_NAME}"
+        return 1
+    fi
+
+    return 0
+}
+
+start_stage() {
+    local STAGE_NAME="$1"
+    rm -f "${STATE_DIR}/${STAGE_NAME}"
+    echo "stage ${STAGE_NAME}"
+}
+
+complete_stage() {
+    local STAGE_NAME="$1"
+    date "+%s" >"${STATE_DIR}/${STAGE_NAME}"
+}
+
+report() {
+    printf "  %s: %s\n" "${STAGE_NAME}" "$@"
+}
+
+SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
+if [[ "${BDR_DIR}" != "${SCRIPT_DIR}" ]]; then
+    abort "update.sh should not be run directly; use setup.sh"
+fi
+
+TEST=false
+FORCE=false
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --test)
+            TEST=true
+            ;;
+        --force)
+            FORCE=true
+            ;;
+        *)
+            abort "unknown argument $1"
+            ;;
+    esac
+    shift
+done
+
+if "${TEST}"; then
+    echo "*** TEST MODE ***"
+fi
+export TEST
+
+STATE_DIR="${BDR_DIR}/state"
+mkdir -p "${STATE_DIR}" || abort "could not create state dir ${STATE_DIR}"
+
+declare -a STAGES
+while IFS= read -r STAGE; do
+    STAGES+=("$STAGE")
+done < <(find "${BDR_DIR}/stages" -type f -print | sort)
+
+if [[ "${#STAGES[@]}" == 0 ]]; then
+    abort "no installation stages found"
+fi
+
+for STAGE in "${STAGES[@]}"; do
+    STAGE_NAME="$(basename -s .sh "${STAGE}")"
+
+    if check_stage "${STAGE_NAME}"; then
+        echo "skipping ${STAGE_NAME}, already complete"
+        continue
+    fi
+
+    start_stage "${STAGE_NAME}"
+    source "${STAGE}"
+    run_stage || abort "stage ${STAGE_NAME} failed"
+    complete_stage "${STAGE_NAME}"
+done
