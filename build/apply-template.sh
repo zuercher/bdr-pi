8c05731d2167c4fc1ca040bb4e744d72d7cb320b
Stephan Zuercher <szuercher@slack-corp.com>
2021-12-27 11:04:02 -0800

    docs

--- build/apply-template.sh
+++ build/apply-template.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+#/# Scans the input for comments of the form:
+#/#   #{{include file.sh}}
+#/# and substitutes the contents of lib/file.sh into the input and
+#/# writes the modified file as the output. If lib/file.sh contains
+#/# lines book-ended with:
+#/#   #{{begin_exclude}}
+#/#   #{{end_exclude}}
+#/# those lines are not copied into the output.
 usage() {
     echo "usage: $0 <input> <output>"
+    echo
+    grep -E "^#/#" "$0" | cut -c 5-
     exit 1
 }
 
 DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-INPUT="$1"
+case "$1" in
+    -h|-help|--help)
+        usage
+        ;;
+    *)
+        INPUT="$1"
+        ;;
+esac
 OUTPUT="$2"
 
 [[ -z "${INPUT}" ]] && usage
@@ -15,7 +32,13 @@ OUTPUT="$2"
 
 TEMP_DIR="$(mktemp -d -t bdr-pi-apply-tmpl)"
 
+# Write sed code to a script: need new lines to allow r and d commands
+# in a single block.
 SCRIPT="${TEMP_DIR}/apply-templates.sed"
+
+# For each script in lib, strip the code in begin_exclude/end_exclude,
+# write the modified file to TEMP_DIR, and add a statement to the sed
+# script to substitute it.
 find "${DIR}/lib" -type f -a -name "*.sh" -print \
     | while read -r FILE; do
     FILE_BASE="$(basename "${FILE}")"
@@ -34,6 +57,7 @@ find "${DIR}/lib" -type f -a -name "*.sh" -print \
 EOF
 done
 
+# Run the generated script against INPUT, writing to OUTPUT.
 if ! sed -f "${SCRIPT}" "${INPUT}" >"${OUTPUT}"; then
     echo "error applying templates"
     exit 1
