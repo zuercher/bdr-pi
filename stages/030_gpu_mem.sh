5f175a08ab7a4cf15f23ab6742035b4b28e4de09
Stephan Zuercher <szuercher@slack-corp.com>
2021-12-28 13:38:33 -0800

    gpu mem (#9)

---
+++ stages/030_gpu_mem.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+run_stage() {
+    if grep -q -E "^gpu_mem=256$" /boot/config.txt; then
+        report "gpu_mem already set to 256 MB, skipping"
+        return 0
+    fi
+
+    report "setting gpu_mem to 256 MB"
+    sed -i "s/^gpu_mem=[0-9]\+$/gpu_mem=256/" /boot/config.txt
+
+    reboot_required
+}
