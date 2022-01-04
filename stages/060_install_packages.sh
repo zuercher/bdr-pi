36adff2551fd0b470ee4b6fd2c017d58e1cec533
Stephan Zuercher <szuercher@slack-corp.com>
2022-01-04 11:23:35 -0800

    lifepo4wered-pi and racecap step1 (#17)

--- stages/060_install_packages.sh
+++ stages/060_install_packages.sh
@@ -6,16 +6,4 @@ run_stage() {
     apt-get update || abort "unable to update apt repositories"
 
     apt-get upgrade -y || abort "unable to update installed packages"
-
-    report "installing additional packages for racecapture"
-
-    local PKGS=(
-        mesa-utils
-        libgles2
-        libegl1-mesa
-        libegl-mesa0
-        mtdev-tools
-    )
-    apt-get install -y "${PKGS[@]}" || abort "unable to install packages: ${PKGS[*]}"
 }
-
---
+++ stages/200_download_racecapture.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+run_stage() {
+    RC_VERSION="${BDRPI_RACECAPTURE_VERSION:-2.2.0}"
+    RC_FILE="${BDRPI_RACECAPTURE_FILE:-racecapture_linux_raspberrypi_${RC_VERSION}.tar.bz2}"
+    RC_URL="${BDRPI_RACECAPTURE_URL:-https://autosportlabs-software.s3-us-west-2.amazonaws.com/${RC_FILE}}"
+
+    report "installing additional packages for racecapture"
+
+    local PKGS=(
+        mesa-utils
+        libgles2
+        libegl1-mesa
+        libegl-mesa0
+        mtdev-tools
+        wget
+    )
+    apt-get install -y "${PKGS[@]}" || abort "unable to install packages: ${PKGS[*]}"
+
+    push_dir "${SETUP_HOME}"
+
+    rm -f "${RC_FILE}"
+
+    # download and extract as the setup user to keep the permissions correct
+    sudo -u "${SETUP_USER}" wget "${RC_URL}" || abort "unable to download ${RC_URL}"
+    report "extracting ${RC_FILE}"
+    sudo -u "${SETUP_USER}" tar xfj "${RC_FILE}" || abort "unable to extract ${RC_FILE}"
+
+    [[ -d "${SETUP_HOME}/racecapture" ]] || abort "missing ${SETUP_HOME}/racecapture directory"
+
+    pop_dir
+}
