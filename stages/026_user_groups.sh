396cc6cf39b5c23b074921bce06b7e37c7026897
Stephan Zuercher <zuercher@gmail.com>
2023-10-25 10:11:39 -0700

    avoid pre-defined GROUPS var

--- stages/026_user_groups.sh
+++ stages/026_user_groups.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 run_stage() {
-    report "adding user ${SETUP_USER} to group render"
+    report "adding user ${SETUP_USER} to groups"
 
-    local GROUPS=(render video input dialout)
-    for GROUP in "${GROUPS[@]}"; do
-        adduser "${SETUP_USER}" "${GROUP}" \
-            || abort "failed to add ${SETUP_USER} to group ${GROUP}"
+    local GROUP_NAMES=(render video input dialout)
+    for GROUP_NAME in "${GROUP_NAMES[@]}"; do
+        adduser "${SETUP_USER}" "${GROUP_NAME}" \
+            || abort "failed to add ${SETUP_USER} to group ${GROUP_NAME}"
     done
 }
