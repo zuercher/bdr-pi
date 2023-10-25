ec19deda8946416a56b4b6bbf983716a26c6165d
Stephan Zuercher <zuercher@gmail.com>
2023-10-25 12:24:51 -0700

    caps lock is control

--- stages/012_keyboard.sh
+++ stages/012_keyboard.sh
@@ -2,17 +2,28 @@
 
 run_stage() {
     local LAYOUT="${BDRPI_KEYBOARD_LAYOUT:-us}"
-
+    local CTRL_OPT="ctrl:nocaps"
     local CONFIG="/etc/default/keyboard"
 
-    if grep -q -F "XKBLAYOUT=\"${LAYOUT}\"" "${CONFIG}"; then
-        report "keyboard layout is already ${LAYOUT}"
+    if (source "${CONFIG}"; [[ "${XKBLAYOUT}" == "${LAYOUT}" ]]) &&
+           (source "${CONFIG}"; echo "${XKBOPTIONS}" | grep -q "${CTRL_OPT}"); then
+        report "keyboard layout and options are already set"
         return 0
     fi
 
     report "setting keyboard layout to ${LAYOUT}"
 
     sed -i "s/^\s*XKBLAYOUT=\S*/XKBLAYOUT=\"${LAYOUT}\"/" "${CONFIG}"
+
+    reprt "setting ${CTRL_OPT} (caps lock is control)"
+
+    local OPTIONS="$(source "${CONFIG}"; echo "${XKBOPTIONS}")"
+    if [[ -z "${OPTIONS}" ]]; then
+        OPTIONS="${CTRL_OPT}"
+    else
+        OPTIONS="${OPTIONS},${CTRL_OPT}"
+    fi
+    sed -i "s/^\s*XKBOPTIONS=\S*/XKBOPTIONS=\"${OPTIONS}\"/" "${CONFIG}"
 
     reboot_required
 }
