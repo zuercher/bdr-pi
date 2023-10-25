d24e94d859ba645c7d3a4a5758d830cbe3dd6abb
Stephan Zuercher <zuercher@gmail.com>
2023-10-25 12:51:40 -0700

    combine and reorder boot options

--- stages/040_gpu_mem.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-run_stage() {
-    local GPU_MEM="${BDRPI_GPU_MEM:-256}"
-
-    boot_config_set "all" "gpu_mem" "${GPU_MEM}"
-}
--- stages/110_boot_options.sh
+++ stages/110_boot_options.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 run_stage() {
+    local GPU_MEM="${BDRPI_GPU_MEM:-256}"
+
     # Disable pi logo, console blanking, and the splash screen
     if ! grep -q "logo\.nologo" /boot/cmdline.txt; then
+        report "disabling boot logo"
+
         sed -i '1 s/$/ logo.nologo/' /boot/cmdline.txt
     fi
+
     if ! grep -q "consoleblank=0" /boot/cmdline.txt; then
+        report "disabling console blanking"
+
         sed -i '1 s/$/ consoleblank=0/' /boot/cmdline.txt
     fi
 
-    # Disable splash screen
-    if ! grep -q "^disable_splash" /boot/config.txt; then
-        echo "disable_splash=1" >> /boot/config.txt
-    fi
+    boot_config_set "all" "disable_splash" "1"
+    boot_config_set "all" "disable_touchscreen" "1"
+    boot_config_set "all" "gpu_mem" "${GPU_MEM}"
 }
