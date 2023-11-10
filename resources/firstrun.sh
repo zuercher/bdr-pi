9909ceaa11893a8994779b5767fe13386e63552e
Stephan Zuercher <szuercher@slack-corp.com>
2023-11-09 17:04:08 -0800

    maybe $USER is special

--- resources/firstrun.sh
+++ resources/firstrun.sh
@@ -49,40 +49,40 @@ getconfig() {
 }
 
 configure_user() {
-    local USER="$1"
-    local PASS="$2"
+    local BDR_USER="$1"
+    local BDR_PASS="$2"
 
     local FIRST_USER
     FIRST_USER="$(getent passwd 1000 | cut -d: -f1)"
-    if [[ -z "${FIRSTUSER}" ]]; then
+    if [[ -z "${FIRST_USER}" ]]; then
         # No user to rename...
         abort "unable to find first user for rename (falling back to userconfig service)"
     fi
 
     # rename user (as in /usr/lib/userconfig-pi/userconfig)
-    if [[ "${FIRST_USER}" != "${USER}" ]]; then
+    if [[ "${FIRST_USER}" != "${BDR_USER}" ]]; then
         # rename user
-        logger "rename ${FIRST_USER} to ${USER}"
+        logger "rename ${FIRST_USER} to ${BDR_USER}"
 
         local FIRST_GROUP
         FIRST_GROUP="$(getent group 1000 | cut -d: -f1)"
 
-        usermod -l "${USER}" "${FIRST_USER}"
-        usermod -m -d "/home/${USER}" "${USER}"
-        groupmod -n "${USER}" "${FIRST_GROUP}"
+        usermod -l "${BDR_USER}" "${FIRST_USER}"
+        usermod -m -d "/home/${BDR_USER}" "${BDR_USER}"
+        groupmod -n "${BDR_USER}" "${FIRST_GROUP}"
         local F
         for F in /etc/subuid /etc/subgid; do
-            sed_inplace "s/^${FIRST_USER}:/${USER}:/" "${F}"
+            sed_inplace "s/^${FIRST_USER}:/${BDR_USER}:/" "${F}"
         done
         if [ -f /etc/sudoers.d/010_pi-nopasswd ]; then
-            sed_inplace "s/^${FIRST_USER} /${USER} /" /etc/sudoers.d/010_pi-nopasswd
+            sed_inplace "s/^${FIRST_USER} /${BDR_USER} /" /etc/sudoers.d/010_pi-nopasswd
         fi
     fi
 
 
     # as in /usr/lib/userconfig-pi/userconfig
     logger "cancel user-service rename"
-    /usr/bin/cancel-rename "${USER}"
+    /usr/bin/cancel-rename "${BDR_USER}"
 
     # disable userconfig.service
     logger "cancel user-service rename"
@@ -92,7 +92,7 @@ configure_user() {
 
     # change passwd (as in as in /usr/lib/userconfig-pi/userconfig-service)
     logger "change user password"
-    echo "${USER}:${PASS}" | chpasswd
+    echo "${BDR_USER}:${BDR_PASS}" | chpasswd
 
     # delete the password from the config
     logger "remove user password from setup config"
@@ -102,44 +102,44 @@ configure_user() {
 logger "starting $0"
 
 # create user with PW from /boot/bdrpi-config.txt
-USER="$(getconfig FIRST_RUN_USER)"
-if [[ -z "${USER}" ]]; then
-    USER="pi"
+BDR_USER="$(getconfig FIRST_RUN_USER)"
+if [[ -z "${BDR_USER}" ]]; then
+    BDR_USER="pi"
     logger "defaulting to user 'pi'"
 fi
 
-PASS="$(getconfig FIRST_RUN_PASS)"
-if [[ -n "${PASS}" ]]; then
-    logger "configuring user ${USER}"
-    configure_user "${USER}" "${PASS}"
+BDR_PASS="$(getconfig FIRST_RUN_PASS)"
+if [[ -n "${BDR_PASS}" ]]; then
+    logger "configuring user ${BDR_USER}"
+    configure_user "${BDR_USER}" "${BDR_PASS}"
 fi
 
-# copy /boot/bdr-setup.sh to /home/$USER
-if [[ ! -f "/home/${USER}/setup.sh" ]]; then
+# copy /boot/bdr-setup.sh to /home/$BDR_USER
+if [[ ! -f "/home/${BDR_USER}/setup.sh" ]]; then
     logger "copying setup script to user home"
-    cp "${BDRPI_SETUP_SH}" "/home/${USER}/setup.sh" || \
+    cp "${BDRPI_SETUP_SH}" "/home/${BDR_USER}/setup.sh" || \
         abort "failed to copy ${BDRPI_SETUP_SH}"
 fi
 
-# append start code to /home/$USER/.bashrc
-if ! grep -q "BEGIN_ON_REBOOT" "/home/${USER}/.bashrc"; then
-    logger "configuring autolaunch of setup script for ${USER}"
-    cat >>"/home/${USER}/.bashrc" << EOF
+# append start code to /home/$BDR_USER/.bashrc
+if ! grep -q "BEGIN_ON_REBOOT" "/home/${BDR_USER}/.bashrc"; then
+    logger "configuring autolaunch of setup script for ${BDR_USER}"
+    cat >>"/home/${BDR_USER}/.bashrc" << EOF
       # BEGIN_ON_REBOOT
-      /home/${USER}/setup.sh
+      /home/${BDR_USER}/setup.sh
       # END_ON_REBOOT
 EOF
 fi
 
 # configure autologin
-logger "configuring autologin for ${USER}"
+logger "configuring autologin for ${BDR_USER}"
 ln -fs \
    /lib/systemd/system/getty@.service \
    /etc/systemd/system/getty.target.wants/getty@tty1.service
 cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF
 [Service]
 ExecStart=
-ExecStart=-/sbin/agetty --autologin ${USER} --noclear %I \$TERM
+ExecStart=-/sbin/agetty --autologin ${BDR_USER} --noclear %I \$TERM
 EOF
 
 # setup wireless iface
