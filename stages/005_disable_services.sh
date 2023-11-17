21a08cb5664feb6206ea9e92214b733770192627
Stephan Zuercher <zuercher@gmail.com>
2023-11-16 17:21:36 -0800

    disable unattended upgrades

--- stages/005_disable_services.sh
+++ stages/005_disable_services.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 run_stage() {
-    report "disabling man-db timer"
+    report "disabling and masking man-db timer"
+    systemctl disable man-db.service
     systemctl disable man-db.timer
+    systemctl mask man-db.service
 
     report "disabling bluetooth services"
     systemctl disable hciuart.service
     systemctl disable bluetooth.service
+
+    report "disabling and masking unattended upgrades"
+    systemctl disable apt-daily.service apt-daily-upgrade.service
+    systemctl disable apt-daily.timer apt-daily-upgrade.timer
+    systemctl mask apt-daily.service apt-daily-upgrade.service
 }
